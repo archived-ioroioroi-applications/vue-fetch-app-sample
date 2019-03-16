6bc93082ec13190acdc06291661465dfe4b72adf
ioroioroi <kazuya.ioroi@gmail.com>
2019-03-16 16:32:54 +0000

    Modify FetchData to be able to fetch by clicking button

--- src/pages/FetchData.vue
+++ src/pages/FetchData.vue
@@ -8,6 +8,7 @@
     </p>
     <!-- methods で定義された関数を, @<Event>="<Method>()" で発火させることができる -->
     <button @click="clear()">clear</button>
+    <button @click="call()">fetch</button>
   </div>
 </template>
 
@@ -21,21 +22,20 @@ export default {
   methods: {
     clear () {
       this.msg = ''
+    },
+    call () {
+      // 非同期で fetch 以下の処理を実行. ここでは対象のURLにGetリクエストを実行. 非同期なので, 画面が表示された時は data() で定義した初期値が表示され, その後に下記レスポンスに入れ替わる様子が見て取れる.
+      fetch('http://www.geonames.org/postalCodeLookupJSON?postalcode=10504&country=US')
+        .then(response => {
+          // レスポンスのjsonをパース
+          return response.json()
+        })
+        .then(json => {
+          this.msg = json.postalcodes[0].adminName1
+        })
+        .catch(() => {
+        })
     }
-  },
-  // created() で, このページが作られたときに以下の fetch を実行
-  created () {
-    // 非同期で fetch 以下の処理を実行. ここでは対象のURLにGetリクエストを実行. 非同期なので, 画面が表示された時は data() で定義した初期値が表示され, その後に下記レスポンスに入れ替わる様子が見て取れる.
-    fetch('http://www.geonames.org/postalCodeLookupJSON?postalcode=10504&country=US')
-      .then(response => {
-        // レスポンスのjsonをパース
-        return response.json()
-      })
-      .then(json => {
-        this.msg = json.postalcodes[0].adminName1
-      })
-      .catch(() => {
-      })
   }
 }
 
